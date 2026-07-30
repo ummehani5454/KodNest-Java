424aa7d8193131c72dafd1be1a8c06b4605f9090
alampur sofiya <alampursofiya@gmail.com>
2026-07-30 12:45:12 +0530

    Day 1 changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,10 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-       //Write your code here
+        //Write your code here
+        System.out.println("Welcome to the java Track");
+        System.out.println("i already know  how to build programming logic");
+        System.out.println("now i am learning how to express logic using java");
     }
 }
