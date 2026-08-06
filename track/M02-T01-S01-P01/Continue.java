810324177992691e3514e3e8796aa23f9b39c69d
alampur sofiya <alampursofiya@gmail.com>
2026-08-06 12:29:06 +0530

    sofiya

---
+++ track/M02-T01-S01-P01/Continue.java
@@ -0,0 +1,13 @@
+
+public class Continue {
+
+    public static void main(String[] args) {
+        for (int j = 0; j <= 10; j++) {
+            if (j == 6) {
+                continue;
+            }
+            System.out.println(j);
+
+        }
+    }
+}
